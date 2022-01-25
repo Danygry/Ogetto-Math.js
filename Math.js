18b11fe05e0aaf2a47858f523339d3c82734759c
Daniele Grilli <81428089+Danygry@users.noreply.github.com>
2022-01-25 15:25:18 +0100

    Update Math.js

--- Math.js
+++ Math.js
@@ -1,13 +1,18 @@
 alert(Math.random()); // numeri casuali
 
+
 alert(Math.ceil(8.9)); // mostra il numero più vicino verso l'alto (9)
+
 
 alert(Math.floor(8.9)); // mostra il numero più vicino verso il basso (8)
 
+
 alert(Math.round(4.1)); // mostra il numero intero più vicino (4)
+
 
 alert(Math.min(22,41,89, -94)); // -94
 
-alert(Math.max(22,41,89, -94)); //41
+
+
 
 
